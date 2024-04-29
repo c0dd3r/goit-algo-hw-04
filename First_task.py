c149615525270b91601f9c097f27ecefaafb1e85
c0dd3r <hmwrkrst@gmail.com>
2024-04-29 08:08:06 +0300

    second attempt

--- First_task.py
+++ First_task.py
@@ -1,34 +1,34 @@
 def total_salary(path):
+    
     try:
+        with open(path, encoding='utf-8') as file:
+            
+            data = file.readlines()
+
         total_salary = 0
-        num_developers = 0
+        number_of_developers = 0
 
-       
-        with open("file.txt", 'r', encoding='utf-8') as file:
+        for row in data:
            
-            for line in file:
-                
-                data = line.strip().split(',')
-                if len(data) == 2:  
-                    salary = int(data[1])
-                    total_salary += salary
-                    num_developers += 1
+            name, salary_str = row.strip().split(',')
 
-        
-        if num_developers > 0:
-            average_salary = total_salary / num_developers
+            salary = int(salary_str)
+
+            total_salary += salary
+
+            number_of_developers += 1
+
+        if number_of_developers > 0:
+            average_salary = total_salary / number_of_developers
         else:
             average_salary = 0
 
-        
         return total_salary, average_salary
-        
+
     except FileNotFoundError:
-        
-        return "File not found", None
+        print(f"Файл не знайдено: {path}")
+        return 0, 0
+
     except Exception as e:
-       
-        return f"Error: {e}", None
-
-
-
+        print(f"Помилка при роботі з файлом: {e}")
+        return 0, 0
